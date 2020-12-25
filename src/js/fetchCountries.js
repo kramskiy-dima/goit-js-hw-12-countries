805ece6981dde62c400d6a373081a1cd0ad30770
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2020-12-25 23:25:18 +0200

    removes unnecessary .then

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,15 +4,13 @@ export default function fetchCountries(searchQuery) {
   if (searchQuery === '') {
     return;
   }
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-    .then(response => {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(
+    response => {
       if (response.ok) {
         return response.json();
       } else {
         notification.error('Ошибка 404');
       }
-    })
-    .then(obj => {
-      return obj;
-    });
+    },
+  );
 }
